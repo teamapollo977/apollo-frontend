41eeef219bd2e60f0418514c036da8cf1e6a31a6
Kauê Reis <kaue0607@gmail.com>
2024-08-30 00:55:48 +1200

    Update navigation menu

--- src/components/ui/navigation-menu.jsx
+++ src/components/ui/navigation-menu.jsx
@@ -33,7 +33,7 @@ NavigationMenuList.displayName = NavigationMenuPrimitive.List.displayName
 const NavigationMenuItem = NavigationMenuPrimitive.Item
 
 const navigationMenuTriggerStyle = cva(
-  "group inline-flex h-10 w-max items-center justify-center rounded-md px-4 py-2 text-sm font-medium transition-colors hover:bg-sunny hover:text-accent-foreground disabled:pointer-events-none disabled:opacity-50 data-[state=open]:bg-sunny/50"
+  "group inline-flex h-10 w-max items-center justify-center rounded-md px-4 py-2 text-sm font-medium transition-colors backdrop-blur-lg hover:bg-sunny hover:text-accent-foreground disabled:pointer-events-none disabled:opacity-50 data-[state=open]:bg-sunny/50"
 )
 
 const NavigationMenuTrigger = React.forwardRef(({ className, children, ...props }, ref) => (
