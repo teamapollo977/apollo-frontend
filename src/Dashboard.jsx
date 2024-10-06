05c7738d62562defe4f80bb65a8e32b779de017c
Kauê Reis <kaue0607@gmail.com>
2024-10-07 04:16:06 +1300

    Fix dashboard button, update dashboard and landing page text

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,15 +1,20 @@
+import { useNavigate } from "react-router-dom";
 import { useAuth } from "./components/authProvider";
 import DefaultButton from "./components/defaultButton";
 import MagneticButton from "./components/magneticButton"
 
 function Dashboard() {
+  const navigate = useNavigate();
   const { userName } = useAuth();
 
   return (
     <div className="flex flex-col gap-5 items-center text-center w-min">
-      <h2 className="text-3xl font-semibold min-w-max">{`Welcome${userName ? ", " + userName : ""}!`}</h2>
-      <h3 className="text-sm mb-5 min-w-[550px]">“The archer is the true weapon, the bow is just a long piece of wood.”<br/><span className="font-semibold">― Sebastien de Castell, Traitor's Blade</span></h3>
-      <DefaultButton classes="min-w-48 h-14 text-lg font-semibold">
+      <h2 className="text-3xl font-semibold min-w-max">{`Welcome${userName ? ", " + userName : " back"}!`}</h2>
+      <h3 className="text-lg mb-5 min-w-[700px]">“The archer is the true weapon, the bow is just a long piece of wood.”<br/><span className="font-semibold">― Sebastien de Castell, Traitor's Blade</span></h3>
+      <DefaultButton
+        classes="min-w-48 h-14 text-lg font-semibold"
+        onClick={() => navigate("/scoring")}
+      >
         Start Shooting
       </DefaultButton>
     </div>
--- src/Landing.jsx
+++ src/Landing.jsx
@@ -6,7 +6,7 @@ function Landing() {
     <div className="flex flex-col items-center">
       <div className="flex flex-col gap-10 items-center text-center w-min">
         <h2 className="text-5xl font-semibold min-w-max">Value Proposition</h2>
-        <h3 className="text-sm">“Archery is about focus, aim, and release. It’s a powerful metaphor for letting go and moving forward in life.”<br/><span className="font-semibold">― John Lee Dumas</span></h3>
+        <h3 className="text-md min-w-[550px]">“Archery is about focus, aim, and release. It’s a powerful metaphor for letting go and moving forward in life.”<br/><span className="font-semibold">― John Lee Dumas</span></h3>
       </div>
       <MagneticButton text="Get Started"/>
     </div>
