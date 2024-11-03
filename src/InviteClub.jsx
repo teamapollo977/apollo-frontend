31dbfb4a95b43686cd965dfe03e5864edcab76ad
Kauê Reis <kaue0607@gmail.com>
2024-11-03 23:16:42 +1300

    Add club invitation page

---
+++ src/InviteClub.jsx
@@ -0,0 +1,67 @@
+import React, { useEffect } from "react";
+import { Label, LabelInputContainer } from "@/components/ui/input/label";
+import { Input } from "@/components/ui/input/input";
+import DefaultForm from "./components/defaultForm";
+import { useForm } from "react-hook-form";
+import { useAuth } from "./components/authProvider";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+
+export default function InviteClub() {
+  const { authToken } = useAuth();
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const {
+    register,
+    handleSubmit,
+    formState: {
+      errors,
+      isSubmitting,
+    }
+  } = useForm();
+
+  const onSubmit = async (data) => {
+    setLoading(true);
+    await fetch(import.meta.env.VITE_API_URL + "/api/Auth/SendOtpToUser/send-otp", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${authToken}`,
+      },
+      body: JSON.stringify(data),
+    }).catch((error) => {
+      throw new Error("There was an error approving the user. Please try again.");
+    }).finally(() => {
+      setLoading(false);
+    });
+  };
+
+  return (
+    <DefaultForm
+      onSubmit={handleSubmit(onSubmit)}
+      isSubmitting={isSubmitting || loading}
+      title="Invite Club"
+      submitText={isSubmitting || loading ? "Loading..." : "Submit"}
+      redirects={[
+        {
+          text: "Invite a club to join Apollo Archery",
+        }
+      ]}
+    >
+      <LabelInputContainer>
+        <Label htmlFor="email">Email</Label>
+        <Input
+          {...register("email", {
+            required: "Email is required",
+          })}
+          id="email"
+          placeholder="deepsouth@archery.com"
+          type="email"
+          disabled={isSubmitting || loading}
+        />
+        {errors.email && <p className="text-accent text-sm">{errors.email.message}</p>}
+      </LabelInputContainer>
+    </DefaultForm>
+  );
+}
